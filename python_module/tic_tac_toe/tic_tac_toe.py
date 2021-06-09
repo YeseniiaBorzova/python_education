dc74238e17d81152b5c6f743df9185e28fa6352a
YeseniiaBorzova <yeseniiaborzova@gmail.com>
2021-06-09 18:52:01 +0300

    rewrited in oop, added ability to clean logs

--- python_module/tic_tac_toe/tic_tac_toe.py
+++ python_module/tic_tac_toe/tic_tac_toe.py
@@ -1,198 +1,211 @@
+import os
 import logging
 from datetime import datetime
 
 
-# Function to print Tic Tac Toe
-def print_tic_tac_toe(values):
-    print("\n")
-    print("\t     |     |")
-    print("\t  {}  |  {}  |  {}".format(values[0], values[1], values[2]))
-    print('\t_____|_____|_____')
+class Board:
+    first_player = None
+    second_player = None
 
-    print("\t     |     |")
-    print("\t  {}  |  {}  |  {}".format(values[3], values[4], values[5]))
-    print('\t_____|_____|_____')
+    def __init__(self):
+        self.first_player_choice = " "
+        self.second_player_choice = " "
+        self.first_player_score = 0
+        self.second_player_score = 0
+        self.cells = [[" ", " ", " "],
+                      [" ", " ", " "],
+                      [" ", " ", " "]]
 
-    print("\t     |     |")
+    def display(self):
+        print(" %s | %s | %s " % (self.cells[0][0], self.cells[0][1], self.cells[0][2]))
+        print(" --------- ")
+        print(" %s | %s | %s " % (self.cells[1][0], self.cells[1][1], self.cells[1][2]))
+        print(" ---------")
+        print(" %s | %s | %s " % (self.cells[2][0], self.cells[2][1], self.cells[2][2]))
 
-    print("\t  {}  |  {}  |  {}".format(values[6], values[7], values[8]))
-    print("\t     |     |")
-    print("\n")
+    @staticmethod
+    def welcome_user():
+        print("Welcome to tic tac toe game!")
 
+    def refresh_screen(self):
+        os.system('clear')
+        self.welcome_user()
+        self.display()
 
-# Function to print the score-board
-def print_scoreboard(score_board):
-    print("\t--------------------------------")
-    print("\t              SCOREBOARD       ")
-    print("\t--------------------------------")
+    def set_first_player_username(self, username):
+        self.first_player = username
 
-    players = list(score_board.keys())
-    print("\t   ", players[0], "\t    ", score_board[players[0]])
-    print("\t   ", players[1], "\t    ", score_board[players[1]])
+    def set_second_player_username(self, username):
+        self.second_player = username
 
-    print("\t--------------------------------\n")
+    def set_first_player_choice(self, choice):
+        self.first_player_choice = choice.upper()
 
+    def set_second_player_choice(self, choice):
+        self.second_player_choice = choice.upper()
 
-# Function to check if any player has won
-def check_win(player_pos, cur_player):
-    # All possible winning combinations
-    soln = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    def get_first_layer_username(self):
+        return self.first_player
 
-    # Loop to check if any winning combination is satisfied
-    for x in soln:
-        if all(y in player_pos[cur_player] for y in x):
-            # Return True if any winning combination satisfies
+    def get_second_player_username(self):
+        return self.second_player
+
+    def get_first_layer_choice(self):
+        return self.first_player_choice
+
+    def get_second_layer_choice(self):
+        return self.second_player_choice
+
+    def update_first_player_score(self):
+        self.first_player_score += 1
+
+    def update_second_player_score(self):
+        self.second_player_score += 1
+
+    def update_score(self, player):
+        if self.first_player_choice == player.upper():
+            self.update_first_player_score()
+        else:
+            self.update_second_player_score()
+
+    def get_first_layer_score(self):
+        return self.first_player_score
+
+    def get_second_player_score(self):
+        return self.second_player_score
+
+    def identify_player(self, player):
+        if self.first_player_choice == player.upper():
+            return self.first_player
+        return self.second_player
+
+    def update_cell(self, cell_coordinates, player):
+        if player != "X" and player != "O":
+            raise ValueError("You can only play with X or O")
+        if self.cells[cell_coordinates[0] - 1][cell_coordinates[1] - 1] == " ":
+            self.cells[cell_coordinates[0] - 1][cell_coordinates[1] - 1] = player
+
+    @staticmethod
+    def clear_logs():
+        file = open('win_log.txt', "w")
+        file.close()
+
+    def is_winner(self, player):
+        if self.cells[0][0] == player and self.cells[1][1] == player and self.cells[2][2] == player:
+            self.update_score(player)
             return True
-    # Return False if no combination is satisfied
-    return False
-
-
-# Function to check if the game is drawn
-def check_draw(player_pos):
-    if len(player_pos['X']) + len(player_pos['O']) == 9:
-        return True
-    return False
-
-
-# Function for a single game of Tic Tac Toe
-def single_game(cur_player):
-    # Represents the Tic Tac Toe
-    values = [' ' for x in range(9)]
-
-    # Stores the positions occupied by X and O
-    player_pos = {'X': [], 'O': []}
-
-    # Game Loop for a single game of Tic Tac Toe
-    while True:
-        print_tic_tac_toe(values)
-
-        # Try exception block for MOVE input
-        try:
-            print("Player ", cur_player, " turn. Which box? : ", end="")
-            move = int(input())
-        except ValueError:
-            print("Wrong Input!!! Try Again")
-            continue
-
-        # Sanity check for MOVE inout
-        if move < 1 or move > 9:
-            print("Wrong Input!!! Try Again")
-            continue
-
-        # Check if the box is not occupied already
-        if values[move - 1] != ' ':
-            print("Place already filled. Try again!!")
-            continue
-
-        # Update game information
-
-        # Updating grid status
-        values[move - 1] = cur_player
-
-        # Updating player positions
-        player_pos[cur_player].append(move)
-
-        # Function call for checking win
-        if check_win(player_pos, cur_player):
-            print_tic_tac_toe(values)
-            print("Player ", cur_player, " has won the game!!")
-            print("\n")
-            return cur_player
-
-        # Function call for checking draw game
-        if check_draw(player_pos):
-            print_tic_tac_toe(values)
-            print("Game Drawn")
-            print("\n")
-            return 'D'
-
-        # Switch player moves
-        if cur_player == 'X':
-            cur_player = 'O'
+        elif self.cells[0][2] == player and self.cells[1][1] == player and self.cells[2][0] == player:
+            self.update_score(player)
+            return True
+        elif self.cells[0][0] == player and self.cells[0][1] == player and self.cells[0][2] == player:
+            self.update_score(player)
+            return True
+        elif self.cells[1][0] == player and self.cells[1][1] == player and self.cells[1][2] == player:
+            self.update_score(player)
+            return True
+        elif self.cells[2][0] == player and self.cells[2][1] == player and self.cells[2][2] == player:
+            self.update_score(player)
+            return True
+        elif self.cells[0][0] == player and self.cells[1][0] == player and self.cells[2][0] == player:
+            self.update_score(player)
+            return True
+        elif self.cells[0][1] == player and self.cells[1][1] == player and self.cells[2][1] == player:
+            self.update_score(player)
+            return True
+        elif self.cells[0][2] == player and self.cells[1][2] == player and self.cells[2][2] == player:
+            self.update_score(player)
+            return True
         else:
-            cur_player = 'X'
+            return False
 
-
-def play_game():
-    print("Player 1")
-    player1 = input("Enter the name : ")
-    print("\n")
-
-    print("Player 2")
-    player2 = input("Enter the name : ")
-    print("\n")
-
-    # Stores the player who chooses X and O
-    cur_player = player1
-
-    # Stores the choice of players
-    player_choice = {'X': "", 'O': ""}
-
-    # Stores the options
-    options = ['X', 'O']
-
-    # Stores the scoreboard
-    score_board = {player1: 0, player2: 0}
-    print_scoreboard(score_board)
-
-    # Game Loop for a series of Tic Tac Toe
-    # The loop runs until the players quit
-    while True:
-
-        # Player choice Menu
-        print("Turn to choose for", cur_player)
-        print("Enter 1 for X")
-        print("Enter 2 for O")
-        print("Enter 3 to Quit")
-
-        # Try exception for CHOICE input
-        try:
-            choice = int(input())
-        except ValueError:
-            print("Wrong Input!!! Try Again\n")
-            continue
-
-        # Conditions for player choice
-        if choice == 1:
-            player_choice['X'] = cur_player
-            if cur_player == player1:
-                player_choice['O'] = player2
-            else:
-                player_choice['O'] = player1
-
-        elif choice == 2:
-            player_choice['O'] = cur_player
-            if cur_player == player1:
-                player_choice['X'] = player2
-            else:
-                player_choice['X'] = player1
-
-        elif choice == 3:
-            print("Final Scores")
-            print_scoreboard(score_board)
-            break
-
+    def is_draw(self):
+        used_cells = 0
+        for cell_row in self.cells:
+            for cell in cell_row:
+                if cell != " ":
+                    used_cells += 1
+        if used_cells == 9:
+            return True
         else:
-            print("Wrong Choice!!!! Try Again\n")
+            return False
 
-        # Stores the winner in a single game of Tic Tac Toe
-        winner = single_game(options[choice - 1])
+    def reset_field(self):
+        for i in range(3):
+            for j in range(3):
+                self.cells[i][j] = " "
 
-        # Edits the scoreboard according to the winner
-        if winner != 'D':
-            now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            logging.basicConfig(filename='win_log.txt', filemode='a', level=logging.INFO)
-            player_won = player_choice[winner]
-            logging.info(f"Winner:{player_won}, time:{now}")
-            score_board[player_won] = score_board[player_won] + 1
+    def play(self):
+        username = input("Enter username for first player \n")
+        self.set_first_player_username(username)
+        choice = input(f"{username}, you want to play as X or O \n").upper()
 
-        print_scoreboard(score_board)
-        # Switch player who chooses X or O
-        if cur_player == player1:
-            cur_player = player2
+        if choice == "X":
+            self.set_first_player_choice("X")
+            self.set_second_player_choice("O")
+        elif choice == "O":
+            self.set_first_player_choice("O")
+            self.set_second_player_choice("X")
         else:
-            cur_player = player1
+            print("Incorrect input")
+
+        username = input("Enter username for second player \n")
+        self.set_second_player_username(username)
+
+        while True:
+            self.refresh_screen()
+            x_choice = tuple(map(int, input("Enter coordinates, where you want to put X \n").split(',')))
+            self.update_cell(x_choice, "X")
+            self.refresh_screen()
+
+            if self.is_winner("X"):
+                now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                logging.basicConfig(filename='win_log.txt', filemode='a', level=logging.INFO)
+                logging.info(f"Winner:{self.identify_player('X')}, time:{now}")
+                print(f"{self.identify_player('X')}, X wins\n")
+                play_again = input("Want to play again Y/N? \n").upper()
+                if play_again == "Y":
+                    self.reset_field()
+                    continue
+                else:
+                    print(f"Final score \nfirst player {self.first_player}:{str(self.first_player_score)} \n"
+                          f"second player {self.second_player}:{str(self.second_player_score)}")
+                    clean_logs = input("Do you want to clean logs? Y/N \n").upper()
+                    if clean_logs == "Y":
+                        self.clear_logs()
+                    break
+
+            if self.is_draw():
+                print("Its a draw!!!\n")
+                play_again = input("Want to play again Y/N? \n").upper()
+                if play_again == "Y":
+                    self.reset_field()
+                    continue
+                else:
+                    print(f"Final score {self.first_player}:{str(self.first_player_score)} \n"
+                          f"{self.second_player}:{str(self.second_player_score)}")
+                    break
+
+            o_choice = tuple(map(int, input("Enter coordinates, where you want to put O \n").split(',')))
+            self.update_cell(o_choice, "O")
+
+            if self.is_winner("O"):
+                now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                logging.basicConfig(filename='win_log.txt', filemode='a', level=logging.INFO)
+                logging.info(f"Winner:{self.identify_player('O')}, time:{now}")
+                print(f"{self.identify_player('O')}, O wins\n")
+                play_again = input("Want to play again Y/N? \n").upper()
+                if play_again == "Y":
+                    self.reset_field()
+                    continue
+                else:
+                    print(f"Final score {self.first_player}:{str(self.first_player_score)} \n"
+                          f"{self.second_player}:{str(self.second_player_score)}")
+                    clean_logs = input("Do you want to clean logs? Y/N \n").upper()
+                    if clean_logs == "Y":
+                        self.clear_logs()
+                    break
 
 
 if __name__ == "__main__":
-    play_game()
+    board = Board()
+    board.play()
