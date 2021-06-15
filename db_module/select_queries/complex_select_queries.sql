0f149a92ba9c10fd8a15af8fd2933ea780317eeb
YeseniiaBorzova <yeseniiaborzova@gmail.com>
2021-06-15 12:40:31 +0300

    Corrected reviewed mistakes

--- db_module/select_queries/complex_select_queries.sql
+++ db_module/select_queries/complex_select_queries.sql
@@ -17,7 +17,7 @@ INSERT INTO potential_customer (email, name, surname, second_name, city) VALUES
 ('email_8@gamil.com', 'name_8', 'surname_8', 'second_name_8', 'city 17'),
 ('email_9@gamil.com', 'name_9', 'surname_9', 'second_name_9', 'city 17'),
 ('email_10@gamil.com', 'name_10', 'surname_10', 'second_name_10', 'city 17');
-1
+--1
 SELECT users.first_name, users.email
 FROM users
 WHERE city = 'city 17'
@@ -25,33 +25,34 @@ UNION
 SELECT potential_customer.name, potential_customer.email 
 FROM potential_customer
 WHERE city = 'city 17';
-2
+--2
 SELECT first_name, email 
 FROM users
 ORDER BY city, first_name ASC;
-3
+--3
 SELECT category_title, COUNT(products.category_id) AS "amount_of_products"
 FROM products
 INNER JOIN categories
 ON categories.category_id = products.category_id
 GROUP BY category_title
 ORDER BY COUNT(products.category_id) DESC;
-4.1
+--4.1
 SELECT product_title 
 FROM products
 WHERE product_id NOT IN (SELECT DISTINCT product_id FROM cart_product);
-4.2
-SELECT product_title FROM products
-WHERE product_id IN (SELECT DISTINCT product_id FROM cart_product
-WHERE cart_id IN (SELECT cart_id FROM orders WHERE order_status_id = 5));
-4.3
+--4.2
+SELECT DISTINCT product_title FROM cart_product
+LEFT JOIN orders USING(cart_id)
+INNER JOIN products USING(product_id)
+WHERE order_status_id = 5;
+--4.3
 SELECT product_title, COUNT(cart_id) FROM products
 INNER JOIN cart_product
 ON cart_product.product_id = products.product_id
 GROUP BY product_title
 ORDER BY COUNT(cart_id) DESC
 LIMIT 10;
-4.4
+--4.4
 SELECT product_title, COUNT(orders.order_id) FROM carts
 INNER JOIN orders
 ON orders.cart_id = carts.cart_id
@@ -63,14 +64,13 @@ WHERE order_status_id <> 5
 GROUP BY product_title
 ORDER BY COUNT(orders.order_id) DESC
 LIMIT 10;
-4.5
-SELECT users.user_id, first_name, last_name, total
-FROM users
-INNER JOIN carts
-ON users.user_id = carts.user_id
-ORDER BY total DESC
+--4.5
+SELECT SUM(total) AS "total_spend_money_on_orders", users.first_name, users.last_name FROM users
+INNER JOIN carts USING(user_id)
+GROUP BY user_id
+ORDER BY COUNT(users.first_name) DESC
 LIMIT 5;
-4.6
+--4.6
 SELECT users.first_name, users.last_name, COUNT(carts.user_id) FROM users
 INNER JOIN carts
 ON users.user_id = carts.user_id
@@ -79,10 +79,9 @@ ON orders.cart_id = carts.cart_id
 GROUP BY users.first_name, users.last_name
 ORDER BY  COUNT(carts.user_id) DESC
 LIMIT 5;
-4.7
-SELECT users.first_name, users.last_name 
-FROM users
-WHERE user_id IN (SELECT user_id FROM carts
-WHERE cart_id NOT IN (SELECT cart_id FROM orders))
-ORDER BY last_name
+--4.7
+SELECT first_name, last_name FROM users 
+INNER JOIN carts USING(user_id)
+LEFT JOIN orders USING(cart_id)
+WHERE order_status_id = 5
 LIMIT 5;
